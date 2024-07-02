6975c1e5cd46feea0f98281f9368d645c05ebdcd
eusouopeu <pedro.caio@ufba.br>
2024-07-02 02:34:06 -0300

    correção de alguns erros de importação na página principal

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,6 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import Produto from "@/components/Produto/produto";
-import './globals.css'
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import Produto from "@/components/Produto/produto"
 
 export default function Home() {
   return (
