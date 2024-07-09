38c04cd59b8291a99638fe93dd67568fe8cf6726
eusouopeu <pedro.caio@ufba.br>
2024-07-09 15:34:49 -0300

    Exclusão de 'lib/carousel' e reimportação de funções ao componente 'carousel'

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Arrow from '@/public/icons/setaCarrossel.svg'
 import Image from 'next/image'; 
 import Slider from 'react-slick';
-import { calculateItemsToShow, responsive } from '@/lib/carousel';
 
 export interface CarouselProps {
   className?: string;
@@ -19,6 +18,54 @@ export default function Carousel({
   ...restProps
 }: CarouselProps) {
   const sliderRef = useRef<Slider>(null);
+
+  const calculateItemsToShow = (containerWidth: any) => {
+    const itemWidth = 235
+    const spacing = 78
+    const availableWidth = containerWidth - spacing
+
+    let maxItemsDesktop = Math.floor(availableWidth / (itemWidth + spacing))
+    let maxItemsTablet = Math.floor(availableWidth / (itemWidth + spacing))
+    let maxItemsMobile = Math.floor(availableWidth / itemWidth)
+
+    maxItemsDesktop = Math.max(maxItemsDesktop, 1)
+    maxItemsTablet = Math.max(maxItemsTablet, 1)
+    maxItemsMobile = Math.max(maxItemsMobile, 1)
+
+    return {
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: maxItemsDesktop,
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: maxItemsTablet,
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: maxItemsMobile,
+      },
+    }
+  }
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  }
 
   useEffect(() => {
     if (sliderRef.current) {
