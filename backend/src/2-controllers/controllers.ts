e2469f971fab9674fc13c41579bf5033548b9f71
eusouopeu <pedro.caio@ufba.br>
2024-07-09 22:55:17 -0300

    criação de rota e função 'get' para achar produto pelo id

--- backend/src/2-controllers/controllers.ts
+++ backend/src/2-controllers/controllers.ts
@@ -128,10 +128,13 @@ export async function get_product_by_id (req:Request, res:Response) {
     const idExists = await prisma.product.findUnique({
       where: { id: Number(id) }
     })
-    if (idExists) {
-      return res.send(idExists)
+
+    if (!idExists) {
+      return console.log('Id de produto inválida')
     }
+    
+    return res.send(idExists)
   } catch (error: any) {
-    console.error('Deu ruim: ', error)
+    console.error('Deu ruim na recuperação do produto pelo id: ', error)
   }
 }
--- backend/src/3-routes/router.ts
+++ backend/src/3-routes/router.ts
@@ -1,12 +1,13 @@
 import express, { Router, json } from "express"
 import cors from 'cors'
-import { get_product_by_name, get_product_by_category, make_product, delete_product,edit_product} from "../2-controllers/controllers"
+import { get_product_by_name, get_product_by_category, make_product, delete_product,edit_product, get_product_by_id } from "../2-controllers/controllers"
 // Roteamento dos processos 
 const router = Router()
 router.use(json())
 router.use(cors())
 // Definir rotas dos produtos abaixo ->
 router.post('/make_product', make_product) 
+router.get('/get_product_by_id', get_product_by_id)
 router.get('/search_by_name', get_product_by_name)
 router.get('/filter_by_tag', get_product_by_category)
 router.put('/edit_product', edit_product)
