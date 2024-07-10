a8cfc93377109c2fac6e1522ecca6cbf6a8fdfb1
cristiano-s-r-filho <cristiano.sr.filho@proton.me>
2024-07-10 01:46:23 -0300

    feat: Adicionada as funcionalidades de imagem - ainda não plenamente testadas

--- backend/src/3-routes/router.ts
+++ backend/src/3-routes/router.ts
@@ -1,15 +1,22 @@
 import express, { Router, json } from "express"
 import cors from 'cors'
-import { get_product_by_name, get_product_by_category, make_product, delete_product,edit_product} from "../2-controllers/controllers"
+import { get_product_by_name, get_product_by_category, make_product, delete_product,edit_product, get_all, add_main_img} from "../2-controllers/controllers"
+import upload from "../2-controllers/multer"
 // Roteamento dos processos 
 const router = Router()
 router.use(json())
 router.use(cors())
 // Definir rotas dos produtos abaixo ->
+// Rotas Post
 router.post('/make_product', make_product) 
+// Rotas Get 
 router.get('/search_by_name', get_product_by_name)
 router.get('/filter_by_tag', get_product_by_category)
+router.get('/get_all',get_all)
+// Rotas Put
+router.put('/add_main', upload.single('image'),add_main_img )
 router.put('/edit_product', edit_product)
+// Rotas Delete
 router.delete('/delete_product', delete_product)
 
 export default router
