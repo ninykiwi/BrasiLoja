3b6db722cf141ff3b723ed6c389c86cd2614b277
cristiano-s-r-filho <cristiano.sr.filho@proton.me>
2024-07-13 16:00:17 -0300

    Rotas de adicionar e pegar informações do backend com imagens em formato url

--- backend/src/exit.ts
+++ backend/src/exit.ts
@@ -1,9 +1,13 @@
 import express, { Express } from "express";
 import router from "./3-routes/router";
+import path from 'path'
+import upload from "./2-controllers/multer";
+import { add_main_img } from "./2-controllers/controllers";
 // Definições do Servidor
 const server:Express = express()
 const port = 3005
 server.use(express.json())
+server.use('/uploads', express.static(path.join(__dirname,'uploads')))
 server.use('/',router)
 // Lancamento do Servidor 
 server.listen(port, () => {
